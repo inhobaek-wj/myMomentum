cf0077863a298031106d18c3c4ad18376084acae
jake <inhobaek.wj@gmail.com>
2019-11-24 23:29:53 +0900

    remove toDo

--- todo.js
+++ todo.js
@@ -15,7 +15,7 @@ function deleteToDo(event) {
     const li = btn.parentNode;
     toDoList.removeChild(li);
 
-    const cleanToDos = toDos.filter(function(todo) {
+    const cleanToDos = toDos.filter(function(toDo) {
         return toDo.id !== parseInt(li.id);
     });
     toDos = cleanToDos;
