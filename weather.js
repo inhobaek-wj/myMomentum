25e71bbbbc040f9f577ea1f43ff907f49df0ede6
jake <inhobaek.wj@gmail.com>
2019-11-27 02:03:46 +0900

    weather api

--- weather.js
+++ weather.js
@@ -1,4 +1,20 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
 const COORDS = "coords";
+
+function getWeather(lat, lon) {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&apiid=${API_KEY}&units=metric`
+    ).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,6 +28,7 @@ function handleGeoSuccess(position) {
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -28,7 +45,8 @@ function loadCoords() {
     if (loadedCoords === null) {
         askForCoords();
     } else {
-
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
